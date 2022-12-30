b17c08348f3468c5d1a4969cf03fb22037fdcb30
umiswing <umiswing@foxmail.com>
2022-12-30 18:53:08 +0800

    first save of emacs config

---
+++ custom.el
@@ -0,0 +1,95 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline success warning error])
+ '(ansi-color-names-vector
+   ["gray35" "#ff8059" "#44bc44" "#d0bc00" "#2fafff" "#feacd0" "#00d3d0" "gray65"])
+ '(awesome-tray-mode-line-active-color "#2fafff")
+ '(awesome-tray-mode-line-inactive-color "#323232")
+ '(beacon-color "#cc6666")
+ '(chart-face-color-list
+   '("#b52c2c" "#4fd100" "#f1e00a" "#2fafef" "#bf94fe" "#47dfea" "#702020" "#007800" "#b08600" "#1f2f8f" "#5f509f" "#00808f"))
+ '(custom-enabled-themes '(modus-operandi))
+ '(custom-safe-themes
+   '("63df1d9126e997cd7f6ef5f7dd61312b0f65bcb18974e199e82d0e72b8576612" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "7dc296b80df1b29bfc4062d1a66ee91efb462d6a7a934955e94e786394d80b71" "3199be8536de4a8300eaf9ce6d864a35aa802088c0925e944e2b74a574c68fd0" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
+ '(exwm-floating-border-color "#646464")
+ '(fci-rule-color "#373b41")
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-themes-fringe-red))
+ '(flymake-note-bitmap '(exclamation-mark modus-themes-fringe-cyan))
+ '(flymake-warning-bitmap '(exclamation-mark modus-themes-fringe-yellow))
+ '(frame-background-mode 'dark)
+ '(highlight-changes-colors nil)
+ '(highlight-changes-face-list '(success warning error bold bold-italic))
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#c0c530")
+     ("TODO" . "#feacd0")
+     ("NEXT" . "#b6a0ff")
+     ("THEM" . "#f78fe7")
+     ("PROG" . "#00d3d0")
+     ("OKAY" . "#4ae2f0")
+     ("DONT" . "#70b900")
+     ("FAIL" . "#ff8059")
+     ("BUG" . "#ff8059")
+     ("DONE" . "#44bc44")
+     ("NOTE" . "#d3b55f")
+     ("KLUDGE" . "#d0bc00")
+     ("HACK" . "#d0bc00")
+     ("TEMP" . "#ffcccc")
+     ("FIXME" . "#ff9077")
+     ("XXX+" . "#ef8b50")
+     ("REVIEW" . "#6ae4b9")
+     ("DEPRECATED" . "#bfd9ff")))
+ '(ibuffer-deletion-face 'modus-themes-mark-del)
+ '(ibuffer-filter-group-name-face 'modus-themes-pseudo-header)
+ '(ibuffer-marked-face 'modus-themes-mark-sel)
+ '(ibuffer-title-face 'default)
+ '(ispell-dictionary nil)
+ '(mini-modeline-face-attr '(:background unspecified))
+ '(org-src-block-faces 'nil)
+ '(package-selected-packages
+   '(lsp-pyright valign diff-hl demap modus-themes elfeed orderless ef-themes blamer rainbow-delimiters vertico consult find-file-in-project docker-tramp magit rime lsp-ui yasnippet helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode))
+ '(pdf-view-midnight-colors '("#ffffff" . "#100f10"))
+ '(sublimity-mode t)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#cc6666")
+     (40 . "#de935f")
+     (60 . "#f0c674")
+     (80 . "#b5bd68")
+     (100 . "#8abeb7")
+     (120 . "#81a2be")
+     (140 . "#b294bb")
+     (160 . "#cc6666")
+     (180 . "#de935f")
+     (200 . "#f0c674")
+     (220 . "#b5bd68")
+     (240 . "#8abeb7")
+     (260 . "#81a2be")
+     (280 . "#b294bb")
+     (300 . "#cc6666")
+     (320 . "#de935f")
+     (340 . "#f0c674")
+     (360 . "#b5bd68")))
+ '(vc-annotate-very-old-color nil)
+ '(widget-link-prefix "[")
+ '(widget-link-suffix "]")
+ '(widget-mouse-face '(highlight widget-button))
+ '(widget-push-button-prefix "[")
+ '(widget-push-button-suffix "]")
+ '(window-divider-mode nil)
+ '(xterm-color-names
+   ["black" "#ff8059" "#44bc44" "#d0bc00" "#2fafff" "#feacd0" "#00d3d0" "gray65"])
+ '(xterm-color-names-bright
+   ["gray35" "#ef8b50" "#70b900" "#c0c530" "#79a8ff" "#f78fe7" "#4ae2f0" "white"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
+ '(line-number ((t (:inherit default :width normal))))
+ '(mode-line ((t (:inherit modus-themes-ui-variable-pitch :box (:line-width 1 :color "#373b41") :weight normal)))))
---
+++ init.el
@@ -0,0 +1,372 @@
+;; Don't show the splash screen
+(setq inhibit-startup-message t)  ; Comment at end of line!
+
+;; Turn off some unneeded UI elements
+(menu-bar-mode -1)  ; Leave this one on if you're a beginner!
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Display line numbers in every buffer
+(global-display-line-numbers-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+(defun init()
+  (interactive)
+  (find-file "~/.emacs.d/init.el")
+  )
+(defun update()
+  (interactive)
+  (load-file "~/.emacs.d/init.el")
+  )
+;; org mode
+(defun nolinum ()
+  (display-line-numbers-mode -1)
+  )
+(defun blog()
+  (interactive)
+  (find-file "~/org/blog.org")
+  )
+(defun note()
+  (interactive)
+  (find-file "~/org/note.org")
+  )
+(defun learn()
+  (interactive)
+  (find-file "~/org/gtd/learn.org")
+  )
+(defun dev()
+  (interactive)
+  (find-file "~/org/gtd/dev_tools.org")
+  )
+(defun vimforward()
+  (interactive)
+  (forward-word)
+  (forward-word)
+  (backward-word)
+  )
+(global-set-key (kbd "M-f") 'vimforward)
+(add-hook 'org-mode-hook 'nolinum)
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+(setq org-log-done t)
+(setq org-agenda-files '("~/org/gtd"))
+(add-hook 'org-mode-hook #'valign-mode)
+
+;;(require 'color-theme-sanityinc-tomorrow)
+;;(color-theme-sanityinc-tomorrow-night)
+
+(setq url-proxy-services '(("http" . "127.0.0.1:7890")
+			   ("https" . "127.0.0.1:7890")))
+(setq socks-proxy "127.0.0.1:7891")
+(setq elfeed-curl-extra-arguments '("-x127.0.0.1:7890"))
+(setq elfeed-feeds '("https://protesilaos.com/master.xml"
+		     "http://xahlee.info/emacs/emacs/blog.xml"
+		     "http://xahlee.info/comp/blog.xml"))
+(global-set-key (kbd "C-x w") 'elfeed)
+
+(use-package rime
+  :custom
+  (default-input-method "rime"))
+(setq rime-user-data-dir "~/.config/fcitx/rime")
+(setq rime-show-candidate 'posframe)
+
+;; lsp
+;(setq lsp-keymap-prefix "C-c l")
+(lsp-treemacs-sync-mode 1)
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+(add-hook 'cuda-mode-hook 'lsp)
+(add-hook 'python-mode-hook 'lsp)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'cuda-mode-hook #'rainbow-delimiters-mode)
+(global-set-key (kbd "C-,") 'lsp-find-definition)
+;;(setenv "PYTHONPATH" "~/miniconda3/envs/pytorch/bin")
+;;(setq lsp-pyls-server-command "/home/wsm/miniconda3/envs/pytorch/bin/pylsp")
+;;(setq lsp-clients-python-library-directories "/home/wsm/miniconda3/envs/pytorch
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
+
+(setq lsp-pyright-venv-path "/home/wsm/miniconda3/envs/pytorch/")
+
+;(setq lsp-clients-clangd-args '("--fallback-style=\"BasedOnStyle: Google\""))
+
+;(setq lsp-clients-clangd-library-directories '("/home/wsm/pytorch/niu_attn/cutlass/include",
+;					       "/home/wsm/pytorch/niu_attn/cutlass/tools/util/include"))
+					       
+(lsp-register-client
+    (make-lsp-client :new-connection (lsp-tramp-connection "clangd")
+                     :major-modes '(c-mode c++-mode cuda-mode)
+                     :remote? t
+                     :server-id 'clangd-remote))
+(lsp-register-client
+    (make-lsp-client :new-connection (lsp-tramp-connection "pylsp")
+                     :major-modes '(python-mode)
+                     :remote? t
+                     :server-id 'pylsp-remote))
+;(lsp-register-client
+; (make-lsp-client :new-connection (lsp-tramp-connection "pyright")
+;		  :major-modes '(python-mode)
+;		  :remote? t
+;		  :server-id 'pyright-remote))
+							
+
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (add-hook 'lsp-mode-hook
+            (lambda () (local-set-key (kbd "TAB") #'lsp-format-region)))
+  (add-hook 'lsp-mode-hook
+	    (lambda () (c-toggle-electric-state -1)))
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
+;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
+;;(helm-mode)
+;;(require 'helm-xref)
+;;(define-key global-map [remap find-file] #'helm-find-files)
+;;(define-key global-map [remap execute-extended-command] #'helm-M-x)
+;;(define-key global-map [remap switch-to-buffer] #'helm-mini)
+
+
+;; consult
+;; Example configuration for Consult
+(use-package consult
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key (kbd "M-."))
+  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key (kbd "M-.")
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; (kbd "C-+")
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;; There are multiple reasonable alternatives to chose from.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. projectile.el (projectile-project-root)
+  ;; (autoload 'projectile-project-root "projectile")
+  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 3. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 4. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+)
+(define-key global-map [remap find-file] #'consult-find)
+
+;; vertico
+;; Enable vertico
+(use-package vertico
+  :init
+  (vertico-mode)
+
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; orderless
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;;brackets
+
+;; highlight matching paren
+(show-paren-mode 1)
+
+;; jump to matching bracket by xah lee
+(defvar xah-brackets '("“”" "()" "[]" "{}" "<>" "＜＞" "（）" "［］" "｛｝" "⦅⦆" "〚〛" "⦃⦄" "‹›" "«»" "「」" "〈〉" "《》" "【】" "〔〕" "⦗⦘" "『』" "〖〗" "〘〙" "｢｣" "⟦⟧" "⟨⟩" "⟪⟫" "⟮⟯" "⟬⟭" "⌈⌉" "⌊⌋" "⦇⦈" "⦉⦊" "❛❜" "❝❞" "❨❩" "❪❫" "❴❵" "❬❭" "❮❯" "❰❱" "❲❳" "〈〉" "⦑⦒" "⧼⧽" "﹙﹚" "﹛﹜" "﹝﹞" "⁽⁾" "₍₎" "⦋⦌" "⦍⦎" "⦏⦐" "⁅⁆" "⸢⸣" "⸤⸥" "⟅⟆" "⦓⦔" "⦕⦖" "⸦⸧" "⸨⸩" "｟｠")
+  "A list of strings, each element is a string of 2 chars, the left bracket and a matching right bracket. Used by `xah-select-text-in-quote' and others.")
+
+(defconst xah-left-brackets
+  (mapcar (lambda (x) (substring x 0 1)) xah-brackets)
+  "List of left bracket chars. Each element is a string.")
+
+(defconst xah-right-brackets
+  (mapcar (lambda (x) (substring x 1 2)) xah-brackets)
+  "List of right bracket chars. Each element is a string.")
+
+(defconst ming-brackets
+  (append xah-left-brackets xah-right-brackets)
+  "List of left and right bracket chars. Each element is a string.")
+
+(defun xah-goto-matching-bracket ()
+  "Move cursor to the matching bracket.
+If cursor is not on a bracket, call `backward-up-list'.
+The list of brackets to jump to is defined by `xah-left-brackets' and `xah-right-brackets'.
+
+URL `http://xahlee.info/emacs/emacs/emacs_navigating_keys_for_brackets.html'
+Version: 2016-11-22"
+  (interactive)
+  (if (nth 3 (syntax-ppss))
+      (backward-up-list 1 'ESCAPE-STRINGS 'NO-SYNTAX-CROSSING)
+    (cond
+     ((eq (char-after) ?\") (forward-sexp))
+     ((eq (char-before) ?\") (backward-sexp ))
+     ((looking-at (regexp-opt xah-left-brackets))
+      (forward-sexp))
+     ((looking-back (regexp-opt xah-right-brackets) (max (- (point) 1) 1))
+      (backward-sexp))
+     (t (backward-up-list 1 'ESCAPE-STRINGS 'NO-SYNTAX-CROSSING)))))
+
+;; move cursor to bracket by xah lee
+(defun xah-backward-left-bracket ()
+  "Move cursor to the previous occurrence of left bracket.
+The list of brackets to jump to is defined by `xah-left-brackets'.
+URL `http://xahlee.info/emacs/emacs/emacs_navigating_keys_for_brackets.html'
+Version 2015-10-01"
+  (interactive)
+  (re-search-backward (regexp-opt xah-left-brackets) nil t))
+
+(defun xah-forward-right-bracket ()
+  "Move cursor to the next occurrence of right bracket.
+The list of brackets to jump to is defined by `xah-right-brackets'.
+URL `http://xahlee.info/emacs/emacs/emacs_navigating_keys_for_brackets.html'
+Version 2015-10-01"
+  (interactive)
+  (re-search-forward (regexp-opt xah-right-brackets) nil t))
+
+(global-set-key (kbd "C-9") 'xah-backward-left-bracket)
+(global-set-key (kbd "C-0") 'xah-forward-right-bracket)
+(global-set-key (kbd "C-8") 'xah-goto-matching-bracket)
+
+;; blamer
+(use-package blamer
+  :ensure t
+  :defer 20
+  :custom
+  (blamer-idle-time 0.3)
+  (blamer-min-offset 70)
+  :custom-face
+  (blamer-face ((t :foreground "#7a88cf"
+                    :background nil
+                    :height 111
+                    :italic t)))
+  :config
+  (global-blamer-mode -1))
+
+(blamer-mode -1)
+
+;; ef-themes
+;;(setq ef-themes-file "~/.emacs.d/ef_themes.el")
+;;(load ef-themes-file)
+
+;; modus-themes
+(setq modus-themes-file "~/.emacs.d/modus_themes.el")
+(load modus-themes-file)
+;; demap
+
+
+;; git-gutter
+
+;; diff-hl
+
+(global-diff-hl-mode)
+
+;; others
+
+;; make backup to a designated dir, mirroring the full path
+(defun xah-backup-nested-dir-file-path (Fpath)
+  "Return a new file path and create dirs.
+If the new path's directories does not exist, create them.
+version 2022-06-09"
+  (let* ($backupRoot $backupFilePath)
+    (setq $backupRoot "~/.emacs.d/backup/")
+    ;; remove Windows driver letter in path, e.g. C:
+    (setq $backupFilePath
+          (format "%s%s~" $backupRoot (replace-regexp-in-string "^[A-Za-z]:/" "" Fpath)))
+    (make-directory
+     (file-name-directory $backupFilePath)
+     (file-name-directory $backupFilePath))
+    $backupFilePath
+    ))
+
+(setq make-backup-file-name-function 'xah-backup-nested-dir-file-path)
+
+;; stop creating those #auto-save# files
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
+;; auto revert/refresh file when change detected
+(global-auto-revert-mode 1)
