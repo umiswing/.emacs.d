05375691a3fa45e638a7cc392a196560332605c5
umiswing <umiswing@foxmail.com>
2023-10-01 23:05:49 +0800

    A very elegant and lightweight config.

---
+++ init.el
@@ -0,0 +1,39 @@
+;; Don't show the splash screen
+(setq inhibit-startup-message t)  ; Comment at end of line!
+
+;; Turn off some unneeded UI elements
+(menu-bar-mode -1)  ; Leave this one on if you're a beginner!
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; avy
+(add-to-list 'load-path "~/.emacs.d/avy")
+(require 'avy)
+
+(set-face-attribute 'default nil :height 160)
+
+(global-diff-hl-mode)
+(diff-hl-margin-mode)
+
+(setq auto-save-default nil)
+(setq auto-save-visited-mode nil)
+
+(setq-default indent-tabs-mode nil)
+
+(defun scroll-up-one-line()
+  "Scroll up one line."
+  (interactive)
+  (scroll-up 1))
+(defun scroll-down-one-line()
+  "Scroll down one line."
+  (interactive)
+  (scroll-down 1))
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+
+(global-set-key (kbd "M-n") 'scroll-up-one-line)
+(global-set-key (kbd "M-p") 'scroll-down-one-line)
+(global-set-key (kbd "C-'") 'avy-goto-char)
+(global-set-key (kbd "C-x C-r") 'recentf-open-files)
